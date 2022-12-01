84336858d6cfb1b448bce2e534c91e55f75bda42
Timothe <timothe@lempire.co>
2022-12-01 21:04:57 +0100

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors:{
+        origin: ["http://localhost:3000","*"],
+        methods: ["GET","POST"],
+    },
+});
+
+io.on("connect", socket => {
+    socket.on("sendMessage", input => {
+        const message ={
+            ...input,
+            id: new Date().getTime().toString(),
+            sendAt: new Date().getTime()
+        }
+        console.log(message)
+        io.emit("receive-message",message)
+    })
+    socket.on("ping", input => {
+        console.log('pong')
+    })
+    
+})
+
+server.listen(3001, () => {
+    console.log('Server is running')
+})
