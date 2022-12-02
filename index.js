0f2d266f42c3a6b7e0c0746b6ae1b5b2adb4972d
Timothe <timothe@lempire.co>
2022-12-02 03:40:10 +0100

    push

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ require('dotenv').config()
 
 const server = http.createServer(app)
 
-const io = new Server(server,{
-    allowRequest: (req, callback) => {
-        const noOriginHeader = req.headers.origin === undefined;
-        callback(null, noOriginHeader);
-      }
-});
-
-io.on("connect", socket => {
+const io = new Server(server, {
+    cors: {
+      origin: ["https://sexinfo.vercel.app/", "http://localhost:3000"],
+      methods: ["GET", "POST"],
+    },
+  });
+  
+  io.on("connect", socket => {
     socket.on("sendMessage", input => {
-        const message ={
-            ...input,
-            id: new Date().getTime().toString(),
-            sendAt: new Date().getTime()
-        }
-        console.log(message)
-        io.emit("receive-message",message)
+      const message = {
+        ...input,
+        id: new Date().getTime().toString(),
+        sendAt: new Date().getTime()
+      }
+      console.log(message)
+      io.emit("receive-message", message)
     })
-})
-
-server.listen(3001, () => {
+  })
+  
+  server.listen(3001, () => {
     console.log('Server is running')
-})
+  })
